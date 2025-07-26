295469ad5d03ae1bd99172d9a0f506f9c8c8a6df
TrongQuyet <super.quyet69@gmail.com>
2025-07-26 15:23:25 +0700

    init auth

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
-        tailwindcss(),
+        react(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: process.env.VITE_HOST || 'localhost',
+            protocol: 'ws',
+            clientPort: 5173,
+        },
+    },
 });
